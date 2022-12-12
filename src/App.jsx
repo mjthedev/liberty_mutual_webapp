044314cd2c2b2562723ab760bd20d0919e8a72ba
mjthedev <i@marrionjohnson.io>
2022-12-12 18:20:17 -0500

    feat: added all blanked components for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
+import { Body } from './components/body/Body'
 
 export default App
 function App() {
   return (
     <>
     <Navbar/>   
+    <Body/>
     </>
   );
   }
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Body = () => {
+  return (
+    <div className="grid grid-rows-4">
+            <div className="h-[550px] border border-black"></div>
+            <div className="h-[400px] border border-black"></div>
+            <div className="h-[400px] border border-black"></div>
+            <div className="h-[400px] border border-black"></div>
+    </div>
+  )
+}
